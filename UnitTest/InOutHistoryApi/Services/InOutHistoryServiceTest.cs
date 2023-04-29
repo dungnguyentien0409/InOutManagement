9cdb1bf16aa84b7f79e91d2bc48262893c61a2d6
Dung Nguyen <dungnguyentien0409@gmail.com>
2023-04-29 18:26:13 +0200

    add unit test for history service

--- UnitTest/InOutHistoryApi/Services/InOutHistoryServiceTest.cs
+++ UnitTest/InOutHistoryApi/Services/InOutHistoryServiceTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using HistoryApi.MappingProfiles;
 using HistoryApi.Implementations;
+using Request;
 
 namespace UnitTest.InOutHistoryApi.Services
 {
@@ -27,8 +28,63 @@ namespace UnitTest.InOutHistoryApi.Services
 
         private void InitData()
         {
+            var histories = new List<InOutHistory>
+            {
+                new InOutHistory
+                {
+                    Id = new Guid("c1d80c7c-4f39-4dbb-a39a-282cbd6ecb91"),
+                    UserName = "AdminUser",
+                    DoorName = "FrontDoor",
+                    ActionStatusName = "TAPIN",
+                },
+                new InOutHistory
+                {
+                    Id = new Guid("59c66064-139d-4362-bc6e-28e4b930950b"),
+                    UserName = "NormalUser",
+                    DoorName = "StorageDoor",
+                    ActionStatusName = "FAILED_TAPIN"
+                },
+                new InOutHistory
+                {
+                    Id = new Guid("90a81095-2e2a-41a9-990d-9d91a7010f83"),
+                    UserName = "AdminUser",
+                    DoorName = "StorageDoor",
+                    ActionStatusName = "TAPIN"
+                },
+                new InOutHistory
+                {
+                    Id = new Guid("d99b207a-3312-4126-9a2c-b6f61fbef400"),
+                    UserName = "NormalUser",
+                    DoorName = "FrontDoor",
+                    ActionStatusName = "TAPIN"
+                }
+            }.AsQueryable<InOutHistory>;
 
+            _unitOfWork.Setup(x => x.InOutHistory.Query()).Returns(histories);
+            _unitOfWork.Setup(x => x.InOutHistory.Add(It.IsAny<InOutHistory>())).Verifiable();
         }
+
+        [Test]
+        [TestCase(null, null, null, 4)]
+        [TestCase("FrontDoor", null, null, 2)]
+        [TestCase(null, "NormalUser", null, 2)]
+        [TestCase(null, null, "TAPIN", 3)]
+        [TestCase("FrontDoor", "AdminUser", "TAPIN", 1)]
+        public void GetInOutHistoriesTest(string? doorName, string? userName, string? actionName, int count)
+        {
+            var request = new InOutHistoryRequest
+            {
+                DoorName = doorName,
+                UserName = userName,
+                ActionStatusName = actionName
+            };
+
+            var result = _historyService.GetInOutHistories(request).Result.Count;
+
+            Assert.That(result, Is.EqualTo(count));
+        }
+
+        public void AddHistoryTest()
     }
 }
 
