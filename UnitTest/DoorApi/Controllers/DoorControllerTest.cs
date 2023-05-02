e494d7a395e373d9fd6cab4a91d30ad8670fff28
Dung Nguyen <dungnguyentien0409@gmail.com>
2023-05-02 13:01:03 +0200

    add more unit test

---
+++ UnitTest/DoorApi/Controllers/DoorControllerTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using DoorApi.Controllers;
+using Domain.Interfaces;
+using DoorApi.Interfaces;
+using DoorApi.MappingProfiles;
+using Common.DoorDto;
+using Requests;
+
+namespace UnitTest.DoorApi.Controllers
+{
+	public class DoorControllerTest
+	{
+		private DoorController _doorController;
+		private Mock<IDoorService> _doorService = new Mock<IDoorService>();
+        private Mock<IIotGatewayService> _iotGatewayService = new Mock<IIotGatewayService>();
+        private Mock<IInOutHistoryService> _historyService = new Mock<IInOutHistoryService>();
+		private Mock<ILogger<DoorController>> _logger = new Mock<ILogger<DoorController>>();
+
+        [SetUp]
+		public void SetUp()
+		{
+            var configuration = new MapperConfiguration(cfg => {
+                cfg.AddProfile(new DtosToViewModelsMappingProfile());
+                cfg.AddProfile(new EntitiesToDtosMappingProfile());
+            });
+            IMapper mapper = new Mapper(configuration);
+
+            _doorController = new DoorController(_doorService.Object,
+                _iotGatewayService.Object, mapper, _logger.Object, _historyService.Object);
+
+            _iotGatewayService.Setup(x => x.SendDoorStatus(It.IsAny<TapDoorDto>())).Verifiable();
+            _historyService.Setup(x => x.SaveToHistory(It.IsAny<TapDoorDto>())).Verifiable();
+
+            _doorService.SetupSequence(x => x.Open(It.IsAny<TapDoorDto>()))
+                .Returns(Task.FromResult(true))
+                .Returns(Task.FromResult(false));
+
+            _doorService.Setup(x => x.CreateDoor(It.IsAny<DoorDto>())).Returns(true);
+        }
+
+        [Test]
+        [TestCase("Test", "Test", "Test", true)]
+        [TestCase("Test", "Test", "Test", false)]
+        public void OpenDoorTest(string userName, string doorName, string tapAction, bool result)
+        {
+            var request = new TapDoorRequest()
+            {
+                UserName = userName,
+                DoorName = doorName,
+                TapAction = tapAction
+            };
+
+            var res = _doorController.Open(request).Result;
+
+            Assert.That(result, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void CreateDoor()
+        {
+            var request = new DoorRequest()
+            {
+                Name = "TestDoor",
+                Description = "Test Door"
+            };
+
+            var result = _doorController.Create(request);
+
+            Assert.That(result, Is.EqualTo(true));
+        }
+	}
+}
+
---
+++ UnitTest/UserApi/Controllers/UserControllerTest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Common.UserDto;
+using Microsoft.AspNetCore.Mvc;
+using Requests;
+using UserApi.Controllers;
+using UserApi.Interfaces;
+using UserApi.MappingProfiles;
+
+namespace UnitTest.UserApi.Controllers
+{
+	public class UserControllerTest
+	{
+		private UserController _userController;
+		private Mock<IUserService> _userService = new Mock<IUserService>();
+		[SetUp]
+		public void SetUp()
+		{
+            var configuration = new MapperConfiguration(cfg => {
+				cfg.AddProfile(new DtosToViewModelsMappingProfile());
+				cfg.AddProfile(new EntitiesToDtosMappingProfile());
+            });
+            IMapper mapper = new Mapper(configuration);
+
+			InitData();
+
+			_userController = new UserController(_userService.Object, mapper);
+        }
+
+		private void InitData()
+		{
+		}
+
+		[Test]
+		[TestCase("myid", "mypassworD1995!", 200)]
+		public void SignUpTest_ReturnOk(string userName, string passWord, int code)
+		{
+			_userService.Setup(x => x.SignUp(It.IsAny<UserInfoDto>()))
+				.Returns(true);
+            var request = new SignupRequest
+			{
+				UserName = userName,
+                Password = passWord
+            };
+
+			var result = _userController.SignUp(request) as OkObjectResult;
+
+			Assert.IsNotNull(result);
+			Assert.That(code, Is.EqualTo(result.StatusCode));
+        }
+
+        [Test]
+        [TestCase("myid", "shortpassword", 400)]
+        public void SignUpTest_ReturnBadRequest(string userName, string passWord, int code)
+        {
+            _userService.Setup(x => x.SignUp(It.IsAny<UserInfoDto>()))
+                .Returns(false);
+            var request = new SignupRequest
+            {
+                UserName = userName,
+                Password = passWord
+            };
+
+            var result = _userController.SignUp(request) as OkObjectResult;
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        [TestCase("myid", "mypassworD1995!", 200)]
+        public void SignInTest_ReturnJWT(string userName, string passWord, int code)
+        {
+            _userService.Setup(x => x.SignIn(It.IsAny<UserInfoDto>()))
+                .Returns("ThisIsJWTToken");
+            var request = new SigninRequest
+            {
+                UserName = userName,
+                Password = passWord
+            };
+
+            var result = _userController.SignIn(request) as OkObjectResult;
+
+            Assert.IsNotNull(result);
+            Assert.That(code, Is.EqualTo(result.StatusCode));
+        }
+
+        [Test]
+        [TestCase("myid", "shortpassword", 400)]
+        public void SignInTest_ReturnBadRequest(string userName, string passWord, int code)
+        {
+            _userService.Setup(x => x.SignIn(It.IsAny<UserInfoDto>()))
+                .Returns("");
+            var request = new SigninRequest
+            {
+                UserName = userName,
+                Password = passWord
+            };
+
+            var result = _userController.SignIn(request) as OkObjectResult;
+
+            Assert.IsNull(result);
+        }
+    }
+}
+
